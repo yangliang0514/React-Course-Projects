19f5b41d16b42a2696c01e29bf503cf7e3856286
Kevin Yang <e510021@gmail.com>
2023-08-26 19:43:13 +0800

    add state to steps

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -1,3 +1,58 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
-  return;
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step <= 1) return;
+    setStep((prev) => prev - 1);
+  }
+
+  function handleNext() {
+    if (step >= 3) return;
+    setStep((prev) => prev + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((prev) => !prev)}>
+        {isOpen ? "Hide" : "Show"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              className={step > 1 ? "btn-active" : "btn-disabled"}
+              onClick={handlePrevious}
+            >
+              previous
+            </button>
+            <button
+              className={step < 3 ? "btn-active" : "btn-disabled"}
+              onClick={handleNext}
+            >
+              next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
