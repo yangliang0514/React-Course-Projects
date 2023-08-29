7afc5ce4f6076db4fdab3454fc5bf5dc5e4ef3e0
Kevin Yang <e510021@gmail.com>
2023-08-29 21:11:13 +0800

    complete the stats footer

--- travel-list/src/Stats.jsx
+++ travel-list/src/Stats.jsx
@@ -1,7 +1,30 @@
-export default function Stats() {
+export default function Stats({ items }) {
+  // if there is nothing in the items array, early return an message
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list!</em>
+      </footer>
+    );
+  }
+
+  const totalItemCount = items.length;
+  const packedItemCount = items.reduce(
+    (acc, item) => (item.packed ? acc + 1 : acc),
+    0
+  );
+  const packedPercentage = (packedItemCount / totalItemCount) * 100;
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (x%) </em>
+      {packedPercentage === 100 ? (
+        <em>You've got everything. Ready to go! </em>
+      ) : (
+        <em>
+          You have {totalItemCount} items on your list, and you already packed{" "}
+          {packedItemCount} ({packedPercentage.toFixed(2)}%)
+        </em>
+      )}
     </footer>
   );
 }
