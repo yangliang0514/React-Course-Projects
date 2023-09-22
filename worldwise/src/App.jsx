8662d010e26c986d5b646b63b234e8ecc437cfa3
Kevin Yang <e510021@gmail.com>
2023-09-23 00:00:04 +0800

    use react router

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import PageNav from "./components/PageNav";
+
 export default function App() {
-  return <div>WorldWise</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <PageNav />
+        <Routes>
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
---
+++ worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,3 @@
+export default function PageNotFound() {
+  return <div>404 Page not found!</div>;
+}
