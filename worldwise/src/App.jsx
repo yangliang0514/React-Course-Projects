14d3f2e5964fc3d7ab8cb728bfc6f62744f78dcd
Kevin Yang <e510021@gmail.com>
2023-11-25 20:01:40 +0800

    implement lazy loading and suspense api

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,47 +1,61 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+// use lazy loading and dynamic import instead
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Homepage = lazy(() => import("./pages/Homepage"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import AuthProvider from "./contexts/AuthContext";
 import Protected from "./components/utils/Protected";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
 export default function App() {
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/app"
-              element={
-                <Protected>
-                  <AppLayout />
-                </Protected>
-              }
-            >
-              {/* this is a index route, which is a default route under "/app"  */}
-              {/* the use of Navigate component basically redirects to another route */}
-              {/* the replace mean it replaces the route with the redirected one */}
-              {/* so when the users click back to the previous page, they won't be redirected again */}
-              <Route index element={<Navigate to="cities" replace />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/app"
+                element={
+                  <Protected>
+                    <AppLayout />
+                  </Protected>
+                }
+              >
+                {/* this is a index route, which is a default route under "/app"  */}
+                {/* the use of Navigate component basically redirects to another route */}
+                {/* the replace mean it replaces the route with the redirected one */}
+                {/* so when the users click back to the previous page, they won't be redirected again */}
+                <Route index element={<Navigate to="cities" replace />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </AuthProvider>
     </>
