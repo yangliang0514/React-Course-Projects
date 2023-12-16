e187b05dd4866306122950f97bebaec1ab0d1ec1
Kevin Yang <e510021@gmail.com>
2023-12-17 01:10:34 +0800

    transitioning to redux toolkit

--- redux-practice/src/features/accounts/accountsSlice.js
+++ redux-practice/src/features/accounts/accountsSlice.js
@@ -1,76 +1,65 @@
-// a slice file is just one part of the store (redux central state)
+import { createSlice } from "@reduxjs/toolkit";
 
 // initial state of an account
-const initialStateAccount = {
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-// reducer functions that exports to a central store file to manage state
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/convertingCurrency":
-      return { ...state, isLoading: true };
-    default:
-      return state;
-  }
-}
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      // we no longer need to return the entire object, just write the logic
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    // this is the implementation if you need to recieve more than one argument
+    // with an action creator and all data processing logic should live here
+    requestLoan: {
+      // this is what the action creater going to recieve
+      prepare(amount, purpose) {
+        // this is what the actual reducer going to recieve as action
+        return { payload: { amount, purpose } };
+      },
 
-// redux action creators (functions that return actions instead of manually writing them by hand),
-// a common practice of using redux
-// these action creators exports to be used in file that modifies these states
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+export default accountSlice.reducer;
+
+// just a temporary work around,
+// not the nest way of writing a thunk middleware with redux toolkit
 export function deposit(amount, currency) {
   if (currency === "USD") return { type: "account/deposit", payload: amount };
-
-  // because we've set the thunk middleware, when returning a function (basically putting a function inside a dispatch)
-  // redux will know that it needs to be executed to get the state value
-  // the function has access to the redux dispatch and getState function (second argument)
   return async (dispatch) => {
-    // set a loading state
     dispatch({ type: "account/convertingCurrency" });
-
-    // api call to convert currency
     const res = await fetch(
       `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
     );
-
     const data = await res.json();
-
-    // dispatch the end result
     dispatch({ type: "account/deposit", payload: data.rates.USD });
   };
-}
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-export function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-export function payLoan() {
-  return { type: "account/payLoan" };
 }
--- redux-practice/src/store.js
+++ redux-practice/src/store.js
@@ -1,22 +1,14 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountsSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
 
-// combines all reducers together into one
-const rootReducer = combineReducers({
-  // this key maps to the state name in the store
-  account: accountReducer,
-  customer: customerReducer,
+// this is the newer way to create a store,
+// it sutomatically creates the combined reducer and sets
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-// creates one central store to manage all states in one place
-// the store is a object that looks like { account: ..., customer: ... } in this case
-// also set the "thunk" as middleware that is being executed between getting the data and storing into the store
-const store = createStore(rootReducer, applyMiddleware(thunk)); // this is a deprecated to create a store
-
-// using the action creators with dispatch to modify the states like this
-// store.dispatch(createCustomer("Kevin Yang", "12390534"));
-// store.dispatch(deposit(5000));
 
 export default store;
